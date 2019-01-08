b9f3867279d27a0f4ca38c984a7a38e298ea8216
LCYchase <1025933459@qq.com>
2019-01-08 21:18:24 +0800

    代码上传
    Signed-off-by: LCYchase <1025933459@qq.com>

---
+++ 3/3/test.c
@@ -0,0 +1,12 @@
+//求Sn = a + aa + aaa + aaaa + aaaaa的前5项之和，其中a是一个数字，
+//例如：2 + 22 + 222 + 2222 + 22222
+
+#include<stdio.h>
+#include<stdlib.h>
+int main() {
+
+	int i;
+
+	system("pause");
+	return 0;
+}
